fcc762f4f9eee7aad11f3f9dfcb44b7e9771884f
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-10-28 17:19:36 -0400

    debug: print configs

--- simple/main.py
+++ simple/main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from hatchet_sdk import Context, Hatchet
 from hatchet_sdk.compute.configs import Compute
 
+from hatchet_sdk.compute.managed_compute import ManagedCompute
+
 load_dotenv()
 
 hatchet = Hatchet()
@@ -28,6 +30,15 @@ def main():
     workflow = ManagedWorkflow()
     worker = hatchet.worker("test-worker", max_runs=1)
     worker.register_workflow(workflow)
+
+    managed_compute = ManagedCompute(
+        worker.action_registry, worker.client, worker.max_runs
+    )
+
+    print(managed_compute.configs)
+    print(worker.action_registry)
+    # await managed_compute.cloud_register()
+
     worker.start()
 
 
